e34d191644d9d97df9e21c9d27fe06ad85b49163
Michal Sinisterra <michaelyosimar43@gmail.com>
2022-07-08 23:37:58 -0500

    Feat: delete tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -15,7 +14,6 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URI),
-    TasksModule,
     UsersModule,
     AuthModule,
   ],
--- src/tasks/dto/update-task.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { CreateTaskDto } from './create-task.dto';
-
-export class UpdateTaskDto extends OmitType(CreateTaskDto, ['userId']) {}
