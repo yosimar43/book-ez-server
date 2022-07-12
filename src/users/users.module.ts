c8e0d210fce26b5ae379568558758c7b53839333
Michal Sinisterra <michaelyosimar43@gmail.com>
2022-07-11 22:16:29 -0500

    Feat: use name with class

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { UserSchema } from './entities/user.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { User, UserSchema } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
-        name: 'User',
+        name: User.name,
         schema: UserSchema,
       },
     ]),
